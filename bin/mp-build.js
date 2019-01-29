014f716d828e1bac7e7a1dd78f656be2950a62d3
NewFuture <github@newfuture.cc>
2019-01-29 22:21:06 +0800

    load config

--- bin/mp-build.js
+++ bin/mp-build.js
@@ -2,41 +2,18 @@
 
 ///@ts-check
 'use strict';
-
-var tasks = require('../src/task');
 var argv = require('yargs').argv;
-var fs = require('fs');
-var minify = require('node-json-minify');
+var tasks = require('../src/task');
+var loadConfig = require('../src/load-config');
 
-if (argv['config']) {
-    var configFile = '' + argv['config'];
-    if (!configFile.startsWith('.') && !configFile.startsWith('/')) {
-        configFile = './' + configFile;
-    }
-    if (!fs.existsSync(configFile)) {
-        console.error(configFile, 'configure file not found!');
-    } else {
-        var json = fs.readFileSync(configFile, 'utf-8');
-        var config = JSON.parse(minify(json));
-        Object.assign(tasks.$config, config);
-    }
-}
+var config = loadConfig(argv['config']);
+Object.assign(tasks.$config, config);
 
 for (var key in argv) {
     if (key !== '_' && key !== '$0' && key !== 'config') {
         tasks.$config[key] = argv[key]
     }
 }
-// if (argv['debug']) {
-//     tasks.$config.debug = true;
-// }
-// if (argv['release']) {
-//     tasks.$config.release = true;
-// }
-
-// if(argv['init']){
-//     // init
-// }else
 
 if (argv._.length === 0) {
     tasks.default(() => { });
---
+++ src/load-config.js
@@ -0,0 +1,50 @@
+///@ts-check
+'use strict';
+// 加载配置
+
+var fs = require('fs');
+var minify = require('node-json-minify');
+var colors = require('ansi-colors');
+var log = require('fancy-log');
+
+var TITLE = colors.cyan('config:');
+var DEFAULT_CONFIG_FILES = [
+    'mpconfig.json',
+    'mpconfig.jsonc',
+    '.mpconfig.json',
+    '.mpconfig.jsonc',
+]
+
+/**
+ * 读取配置
+ * @param {string|any} [configFile] 
+ */
+function loadConfig(configFile) {
+    if (configFile) {
+        if (!fs.existsSync(configFile)) {
+            log.error(TITLE, colors.red.underline(configFile), colors.redBright('does not exist'));
+            process.exit(1);
+        }
+    } else {
+        // try load default configure file
+        for (let index = 0; index < DEFAULT_CONFIG_FILES.length; index++) {
+            if (fs.existsSync(DEFAULT_CONFIG_FILES[index])) {
+                configFile = DEFAULT_CONFIG_FILES[index];
+                break;
+            }
+        }
+    }
+    if (configFile) {
+        try {
+            var json = fs.readFileSync(configFile, 'utf-8');
+            var config = JSON.parse(minify(json));
+            log.info(TITLE, 'load configuration from', colors.blue.underline(configFile))
+            return config;
+        } catch (ex) {
+            log.error(TITLE, colors.red.underline(configFile), 'failed to load.', colors.redBright(ex));
+            process.exit(1);
+        }
+    }
+}
+
+module.exports = loadConfig;
