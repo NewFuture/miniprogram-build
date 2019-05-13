51b8f00beb1ba50f12a5b727936eff991eff56dc
NewFuture <github@newfuture.cc>
2019-05-14 02:08:07 +0800

    feat(wxss): import wxss from npm

--- src/lib/wxss-importer.js
+++ src/lib/wxss-importer.js
@@ -25,16 +25,15 @@ function importNpmModule(moduleName) {
         return typeof pkg[key] === "string";
     });
     if (key || (pkg.main && !pkg.main.endsWith('.js'))) {
-        console.log(key, pkg.main, path.join(modulePath, pkg[key || 'main']));
+        // console.log(key, pkg.main, path.join(modulePath, pkg[key || 'main']));
         return path.join(modulePath, pkg[key || 'main']);
     }
 
     // 无有效字段自动搜索 index|style.[scss|sass|wxss|csss]
-    const name = ['index.scss', 'index.sass', /*'index.wxss'*/, 'index.css', 'style.scss', 'style.sass', /*'style.wxss'*/, 'style.css',].find(
-        function (name) {
-            return fs.existsSync(path.join(modulePath, name));
-        }
-    )
+    const name = [
+        'index.scss', 'index.sass', 'index.wxss', 'index.css',
+        'style.scss', 'style.sass', 'style.wxss', 'style.css',
+    ].find(name => fs.existsSync(path.join(modulePath, name)));
     if (name) {
         return path.join(modulePath, name);
     }
@@ -42,15 +41,17 @@ function importNpmModule(moduleName) {
     // 尝试搜索 bower.json
     const bower = fs.existsSync(path.join(modulePath, 'bower.json')) && npm.loadPackage(modulePath, 'bower.json');
     if (bower) {
-        const key = ['sass', 'scss', 'wxss', 'css', 'style'].find(function (key) {
-            return typeof bower[key] === "string";
-        });
+        const key = ['sass', 'scss', 'wxss', 'css', 'style'].find(key => typeof bower[key] === "string");
         if (key || (bower.main && !bower.main.endsWith('.js'))) {
             return path.join(modulePath, bower[key || 'main'])
         }
     }
 }
 
+/**
+ * 自动创建scss文件
+ * @param {string} file 
+ */
 function syncFile(file) {
     // console.log(file);
     if (
@@ -71,8 +72,9 @@ function syncFile(file) {
  * @param {function} done, 
  */
 module.exports = function importer(url, prev, done) {
-    // console.log('importer', url)
+    // console.log('import', url, prev);
     if (url[0] === '~' && url[1] !== '/') {
+        // import from node_modules
         const npmModule = url.substr(1);
         const n = npmModule.split('/').length;
         if (n === 1 || (n === 2 && npmModule[0] === '@')) {
@@ -83,13 +85,22 @@ module.exports = function importer(url, prev, done) {
                     file: syncFile(file),
                 }
             }
-            // console.log('file', file, fs.readFileSync(file).toString());
-
         } else {
-            return { file: path.join(process.cwd(), 'node_modules', npmModule) }
+            const file = path.join(process.cwd(), 'node_modules', npmModule);
+            return { file: syncFile(file) }
         }
-    } else if (url.startsWith("/") && url.endsWith(".wxss")) {
-        return { contents: "@import url(" + url.trim() + ");" }
+    } else if (prev && prev.indexOf('node_modules') >= 0) {
+        // inner import of node_modules
+        // 引用的 node_modules 再次引用其他文件
+        const file = path.resolve(path.dirname(prev), url);
+        return { file: syncFile(file) }
+    } else if (url.endsWith(".wxss") && !fs.existsSync(url + '.scss') && !fs.existsSync(url + '.css') && !fs.existsSync(url + '.sass')) {
+        // 引用本地wxss 并保持不变
+        // keep import 
+        // console.log(url);
+        // dart-sass not support
+
+        return { contents: '@import url(' + url.trim() + ');' };
     } else {
         return null;
     }
