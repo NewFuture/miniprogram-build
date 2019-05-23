1095bd07a57b776bb32602be8c704ec7f4a4761a
NewFuture <github@newfuture.cc>
2019-05-24 03:29:15 +0800

    feat(clean): keep project.config.json when clean

--- src/tasks/clean.js
+++ src/tasks/clean.js
@@ -1,16 +1,25 @@
 ///@ts-check
+//@ts-check
 'use strict';
-// var del = require('del');
 
-var log = require('../log/logger');
-var color = require('../log/color');
+const path = require('path');
+
 const rm = require('rimraf');
+const colors = require('ansi-colors');
 
+const log = require('../log/logger');
+const color = require('../log/color');
 
 exports.build = function (config) {
     return function (cb) {
-        log(color('clean:'), config.dist);
-        rm(config.dist,cb);
+        const projectJson = path.join(config.dist, 'project.config.json')
+        const appJson = path.join(config.dist, 'app.json')
+        log(color('clean:'),  colors.dim('delete'), colors.bold(config.dist), colors.dim.gray(`[exclude: ${projectJson},${appJson}]`));
+        rm(`${config.dist}/**/*`, {
+            glob: {
+                ignore: [projectJson, appJson]
+            }
+        }, cb);
         // return (config.dist);
     };
 }
