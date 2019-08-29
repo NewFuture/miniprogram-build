c3ebc8f9028e1ae4e218b15707117b65264d9d1e
NewFuture <github@newfuture.cc>
2019-08-29 23:00:57 +0800

    feat(devtool): 自动预览和检查

--- src/tasks/devtool.js
+++ src/tasks/devtool.js
@@ -11,6 +11,9 @@ const RAW_TITLE = 'devtool:';
 const TITLE = require('../log/color')(RAW_TITLE);
 const errLog = require('../log/error')(RAW_TITLE);
 
+/**
+ * 打开项目
+ */
 exports.open = function () {
     logger.info(TITLE, 'opening project ' + colors.underline.gray(config.dist))
     return devtool.cli('--open', path.resolve(config.dist))
@@ -23,36 +26,54 @@ exports.open = function () {
         });
 }
 
+/**
+ * 关闭项目
+ */
 exports.close = function () {
     logger.info(TITLE, 'closing project ' + colors.underline.gray(config.dist))
-    return devtool.cli('--close', path.resolve(config.dist))
-        .then(function (res) {
-            if (res.stderr) {
-                errLog({ name: '微信开发工具关闭项目出错', message: res.stderr.trim() });
-            } else {
-                return new Promise(resolve => setTimeout(resolve, 3000));
-            }
-        }).then(function (res) {
-            logger.info(TITLE, '[√] 已经关闭项目 ' + config.dist);
-            return res;
-        });
+    return exports.isOpenPort().then(function (isOpen) {
+        if (isOpen) {
+            return devtool.cli('--close', path.resolve(config.dist))
+                .then(function (res) {
+                    if (res.stderr) {
+                        errLog({ name: '微信开发工具关闭项目出错', message: res.stderr.trim() });
+                    } else {
+                        return new Promise(resolve => setTimeout(resolve, 3000));
+                    }
+                }).then(function (res) {
+                    logger.info(TITLE, '[√] 已经关闭项目 ' + config.dist);
+                    return res;
+                });
+        }
+    })
+
 }
 
+/**
+ * 退出
+ */
 exports.quit = function () {
     logger.info(TITLE, 'quit...')
-    return devtool.cli('--quit', path.resolve(config.dist))
-        .then(function (res) {
-            if (res.stderr) {
-                errLog({ name: 'fail to call wechatdevtools CLI', message: res.stderr.trim() });
-            } else {
-                return new Promise(resolve => setTimeout(resolve, 3000));
-            }
-        }).then(function (res) {
-            logger.info(TITLE, '[√] 已经退出微信开发工具');
-            return res;
-        });
+    return exports.isOpenPort().then(function (isOpen) {
+        if (isOpen) {
+            return devtool.cli('--quit', path.resolve(config.dist))
+                .then(function (res) {
+                    if (res.stderr) {
+                        errLog({ name: 'fail to call wechatdevtools CLI', message: res.stderr.trim() });
+                    } else {
+                        return new Promise(resolve => setTimeout(resolve, 3000));
+                    }
+                }).then(function (res) {
+                    logger.info(TITLE, '[√] 已经退出微信开发工具');
+                    return res;
+                });
+        }
+    });
 }
 
+/**
+ * 上传
+ */
 exports.upload = function () {
     const version = devtool.getPkgVersion() || '0.0.0';
     const uploadProject = version + '@' + path.resolve(config.dist);
@@ -81,6 +102,76 @@ exports.upload = function () {
         })
 }
 
-// gulp.task('close', () => devtool.cli('--close', $config.dist).then(() => new Promise(resolve => setTimeout(resolve, 3000))));
-// gulp.task('quit', () => devtool.cli('--quit').then(() => new Promise(resolve => setTimeout(resolve, 3000))));
-// gulp.task('upload',)
+/**
+ * 自动预览
+ */
+exports.autopreview = function () {
+    //cli --auto-preview /Users/username/demo --auto-preview-info-output /Users/username/info.json
+    logger.info(TITLE, 'auto preview project: ' + config.dist)
+    return devtool.cli('--auto-preview', path.resolve(config.dist))
+        .then(function (res) {
+            if (res.stderr) {
+                errLog({ name: 'fail to call wechatdevtools CLI', message: res.stderr.trim() });
+            } else {
+                return new Promise(resolve => setTimeout(resolve, 3000));
+            }
+        }).then(function (res) {
+            logger.info(TITLE, '[√] 已更新');
+            return res;
+        });
+}
+
+
+/**
+ * 安装提示
+ */
+function warnNotInstelled() {
+    logger.warn(TITLE, colors.yellowBright.bold('[!] ') + colors.yellowBright('小程序开发工具未安装'))
+    logger.warn(TITLE, colors.yellowBright('    下载安装 https://developers.weixin.qq.com/miniprogram/dev/devtools/download.html'))
+    logger.warn(TITLE, colors.yellowBright.bold('[!] ') + colors.yellowBright('Wechat devtools not found'))
+    logger.warn(TITLE, colors.yellowBright('    download link https://developers.weixin.qq.com/miniprogram/en/dev/devtools/download.html'))
+}
+
+
+/**
+ * 是否安装
+ */
+exports.isCliInstalled = function () {
+    return devtool.getCLIPath(function (path) {
+        if (!path) {
+            warnNotInstelled()
+        }
+    }).catch(() => {
+        warnNotInstelled();
+        return false;
+    })
+}
+
+/**
+ * 是否安装
+ */
+exports.isCliInstalled = function () {
+    return devtool.getCLIPath(function (path) {
+        if (!path) {
+            warnNotInstelled()
+        }
+        return !!path;
+    }).catch(() => {
+        return false;
+    })
+}
+
+/**
+ * 是否安装
+ */
+exports.isOpenPort = function () {
+    return devtool.getPort(function (port) {
+        if (!port) {
+            logger.warn(TITLE, '[skip] 微信开发工具端口未开启或启动 已自动跳过...');
+        }
+        return port > 0;
+    }).catch(() => {
+        logger.warn(TITLE, '[skip] 微信开发工具端口未开启或启动 已自动跳过...');
+        return false;
+    })
+}
